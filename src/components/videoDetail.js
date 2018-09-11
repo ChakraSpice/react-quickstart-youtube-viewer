16f761af8c3bc3bca2d820ef25720ea5ea577fbc
Damir Bajramovic <bajramovic.damir@hotmail.com>
2018-09-11 10:13:31 +0200

    Fix a typo

--- src/components/videoDetail.js
+++ src/components/videoDetail.js
@@ -10,8 +10,8 @@ const VideoDetail = ({video}) => {
 
     return (
         <div className="video-detail col-md-8">
-            <div className="embed-responsive embed-reponsive-16by9">
-                <iframe sandbox="allow-scripts" className="embed-responsive-item" src={url}></iframe>
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" src={url}></iframe>
             </div>
             <div className="details">
                 <div>{video.snippet.title}</div>
